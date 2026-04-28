b158f5be3c353f7da7bb224364ba5316b086685f
Paula-Arias11 <pauarijim@alum.us.es>
2026-04-28 20:27:42 +0200

    :)

---
+++ src/main/java/com/videominer/dailymotion/DailymotionApplication.java
@@ -0,0 +1,13 @@
+package com.videominer.dailymotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailymotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailymotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videominer/dailymotion/DailymotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videominer.dailymotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailymotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
