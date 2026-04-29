02297f354e6414c6d381c76b739ca884c30e2671
Paula-Arias11 <pauarijim@alum.us.es>
2026-04-29 18:53:11 +0200

    el service hecho

--- src/main/java/aiss/dailymotionminer/service/DailymotionService.java
+++ src/main/java/aiss/dailymotionminer/service/DailymotionService.java
@@ -9,6 +9,15 @@ public class DailymotionService {
     private final RestTemplate restTemplate= new RestTemplate();
 
     public DailymotionVideo getVideo(String videoId){
-        String url = "https://api.dailymotion.com/video/" + videoId + "?fields=id,title,description,created_time"
+        String url = "https://api.dailymotion.com/video/" + videoId + "?fields=id,name,description,releaseTime";
+        return restTemplate.getForObject(url, DailymotionVideo.class);
+    }
+    public DailymotionVideoList getVideosFromUser(String userId){
+        String url= "https://api.dailymotion.com/user/" + userId + "/videos";
+        return restTemplate.getForObject(url, DailymotionVideoList.class);
+    }
+    public DailymotionChannel getChanel(String userId){
+        String url = "https://api.dailymotion.com/user/" + userId + "?fields=id,name,description";
+        return restTemplate.getForObject(url, DailymotionChannel.class);
     }
 }
